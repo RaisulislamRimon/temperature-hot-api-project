5ebfde65a0c852ff96a33146bd00dc87a1f4aaf8
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-17 09:40:36 +0000

    added city name & city temperature

--- js/index.js
+++ js/index.js
@@ -8,6 +8,15 @@ searchBtn.addEventListener("click", () => {
     `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => displayWeather(data))
     .catch((err) => console.log(err));
 });
+
+const displayWeather = (data) => {
+  const weatherImg = document.getElementById("weather-img");
+  const cityName = document.getElementById("city-name");
+  const temperature = document.getElementById("temperature");
+
+  cityName.innerText = data.name;
+  temperature.innerText = data.main.temp;
+};
