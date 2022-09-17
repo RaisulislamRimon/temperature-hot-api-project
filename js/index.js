1c938c7e6cede356418f473989f76897e1f07acb
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-17 09:13:06 +0000

    favicon & open weather api added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+const apiKey = "b0177670c5173ceba5d7a1e2a15b3154";
+
+searchBtn.addEventListener("click", () => {
+  const searchValue = searchInput.value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
